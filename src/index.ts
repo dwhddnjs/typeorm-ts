6890b817a557f1220e9134576e209a47c95454ac
Jongwon <per1215@naver.com>
2022-09-12 20:00:35 +0900

    ✨ routes 추가합니다

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { User } from "./entity/User";
 import { AppDataSource } from "./data-source";
-import * as express from "express";
+import { Response, Request } from "express";
+import express = require("express");
 import * as cors from "cors";
 
 const app = express();
@@ -9,18 +10,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await AppDataSource.getRepository(User).find();
+  res.json(users);
+});
+app.post("/signin", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    email: email,
+    password: password,
+  });
+  if (user) {
+    return res.send("유저가 존재합니다");
+  } else {
+    return res.send("아디 비번이 틀립니다");
+  }
+});
+
 AppDataSource.initialize()
-  .then(async () => {
-    const user = new User();
-    user.email = "syd1215no@naver.com";
-    user.password = "a102030a";
-    await AppDataSource.manager.save(user);
-
-    const users = await AppDataSource.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    app.listen(() => {
-      console.log("서버 실행중입니다");
-    });
+  .then(() => {
+    console.log("DB연결 완료");
   })
   .catch((error) => console.log(error));
+
+app.listen(4000, () => {
+  console.log("서버 실행중입니다");
+});
