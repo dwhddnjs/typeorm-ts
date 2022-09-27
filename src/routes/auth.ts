db39cd8adc365c62bba52c0bdeae21dec9272d0c
Jongwon <per1215@naver.com>
2022-09-27 21:46:32 +0900

    🔥 불필요한 코드 제거합니다

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,23 +9,19 @@ const router = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 
 router.post("/register", async (req, res) => {
-  //회원생성 전 유효성검사
   const { email, name, password } = req.body;
 
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  //회원이 존재하는지 확인하기
   const emailExist = await userRepository.findOneBy({
     email: email,
   });
   if (emailExist) return res.status(400).send("해당 이메일이 이미 존재합니다");
 
-  //패스워드 암호화
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  //회원생성
   const user = new User();
   user.email = email;
   user.name = name;
